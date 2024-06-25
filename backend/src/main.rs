10ed41667424e1f97e6faaa4cbc4596f0f50e257
keisas <hurisasi0818@gmail.com>
2024-06-25 22:46:16 +0900

    データベースとの接続

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, web::Data};
+use sqlx::postgres::PgPoolOptions;
+// use sqlx::PgPool;
+use std::env;
+use dotenv::dotenv;
 
 async fn search() -> impl Responder {
     HttpResponse::Ok().body("Hello, this is the search endpoint!")
@@ -7,8 +11,18 @@ async fn search() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(Data::new(pool.clone()))
             .route("/search", web::get().to(search))
     })
     .bind("127.0.0.1:8080")?
