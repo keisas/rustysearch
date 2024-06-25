71c32282081390b9a847335f1da1d14e8f98c7f7
keisas <hurisasi0818@gmail.com>
2024-06-25 22:18:29 +0900

    簡易なapiの作成

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn search() -> impl Responder {
+    HttpResponse::Ok().body("Hello, this is the search endpoint!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/search", web::get().to(search))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
